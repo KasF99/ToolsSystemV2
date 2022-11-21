9fd8a53b5c52383b7d7612043d306d54abdf7115
KasF99 <kasprzyk.filip.99@gmail.com>
2022-11-21 11:31:55 +0100

    Added photo gallery to the tools details component

--- client/src/app/tools/tools-detail/tools-detail.component.ts
+++ client/src/app/tools/tools-detail/tools-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Tool } from 'src/app/_models/tools';
 import { ToolService } from 'src/app/_services/tool.service';
 
@@ -11,16 +12,44 @@ import { ToolService } from 'src/app/_services/tool.service';
 export class ToolsDetailComponent implements OnInit {
   tool: Tool
   closeToService: boolean = false
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(public toolService: ToolService, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.loadTool()
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+
+    
+  }
+
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const photo of this.tool.photos) {
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url
+      })
+    }
+    return imageUrls;
   }
 
   loadTool() {
     this.toolService.getTool(this.route.snapshot.paramMap.get('toolname')).subscribe(tool => {
       this.tool = tool
+      this.galleryImages = this.getImages();
       this.close()
     })
     
