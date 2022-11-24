d7c58159016b2a296d76df27c94bc9fbd2a6b321
KasF99 <kasprzyk.filip.99@gmail.com>
2022-11-24 10:55:05 +0100

    Added tool update(admin) functionality - client

--- client/src/app/admin-panel/tools/tools-edit-admin/tools-edit-admin.component.ts
+++ client/src/app/admin-panel/tools/tools-edit-admin/tools-edit-admin.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Tool } from 'src/app/_models/tools';
 import { ToolService } from 'src/app/_services/tool.service';
 
@@ -9,9 +11,11 @@ import { ToolService } from 'src/app/_services/tool.service';
   styleUrls: ['./tools-edit-admin.component.css']
 })
 export class ToolsEditAdminComponent implements OnInit {
-
+  @ViewChild('editForm') editForm: NgForm| undefined
   tool: Tool
-  constructor(public toolService: ToolService, public route: ActivatedRoute) { }
+
+  constructor(public toolService: ToolService, public route: ActivatedRoute,
+    public toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.loadTool()
@@ -22,6 +26,12 @@ export class ToolsEditAdminComponent implements OnInit {
       this.tool = tool
     })
     
+  }
+
+  updateTools() {
+    this.toastr.success('Profile updated successfully');
+    console.log(this.tool)
+    this.editForm?.reset(this.tool)
   }
 
 }
